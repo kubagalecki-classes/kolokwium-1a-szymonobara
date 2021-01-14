7c4738c407c1409606810ccde189e74687d1581e
szymonobara <Szymon2904@gmail.com>
2021-01-14 12:09:05 +0000

    zad2

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,3 +3,10 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
+obliczMaka(const std::vector<Tagliatelle> v)
+{
+  double M;
+  M=ileMaki(v.end())+ileMaki(v[v.size()-1])+ileMaki(v[v.size()-2]);
+  if (M>100)
+    
+}
