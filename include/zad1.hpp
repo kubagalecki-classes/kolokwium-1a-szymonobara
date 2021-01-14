c4cfe2d478468f92f7139a6a4b466e4217b08642
szymonobara <Szymon2904@gmail.com>
2021-01-14 11:34:01 +0000

    x

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -3,6 +3,25 @@
 // Zad2
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
-
+class Makaron
+{
+  void gotujMakaron();
+};
 // Zad1
 // tutaj definicja klasy Tagliatelle
+class Tagliatelle
+{
+public:
+	Tagliatelle(double L_, double W_, double R_): L(L_), W(W_), R(R_) {}
+	Tagliatelle(): L(0.5), R(0.5), W(0.5) {}
+
+  double ileMaki(unsigned P) const
+	{
+		return P * L*W*(1. - R)*C;
+	}
+private:
+	double L;
+	double W;
+	double R;
+	static const double C;
+};
