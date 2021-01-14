d88fdb4f6c76630f292b016aeea1cd8f456b964f
szymonobara <Szymon2904@gmail.com>
2021-01-14 12:36:50 +0000

    zad5

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -8,7 +8,7 @@
 
 // tutaj funkcja sortujTagliatelle
 template<typename T>
-void sortujTagliatell(T t1, T t2)
+void sortujTagliatelle(T t1, T t2)
 {
   std::sort(t1,t2,[&](Tagliatelle tag1, Tagliatelle tag2){
     return tag1.ileMaki(4)<tag2.ileMaki(4);
