de81c79962f39e96073b25b9b69ffa0333f48eba
szymonobara <Szymon2904@gmail.com>
2021-01-14 12:17:32 +0000

    zad2

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -9,7 +9,7 @@ class Makaron
   public:
   virtual ~Makaron() = default;
   virtual double ileMaki(unsigned) const=0;
-  Makaron* gotujMakaron(const std::string& s);
+  Makaron* gotujMakaron();
 };
 // Zad1
 // tutaj definicja klasy Tagliatelle
